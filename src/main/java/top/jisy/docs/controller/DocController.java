8c710f5635784cc871e9fd1a20f7ebec674f48e9
94rain <94rain@msn.com>
2020-11-04 17:37:15 +0800

    update

---
+++ src/main/java/top/jisy/docs/controller/DocController.java
@@ -0,0 +1,21 @@
+package top.jisy.docs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.jisy.docs.pojo.Doc;
+import top.jisy.docs.service.DocService;
+
+import java.util.List;
+
+@RestController
+public class DocController {
+
+    @Autowired
+    DocService docService;
+
+    @GetMapping("/doc/getAll")
+    public List<Doc> getDoc() {
+        return docService.getDocs();
+    }
+}
